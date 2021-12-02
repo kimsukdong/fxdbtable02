b68e4431e18620210fc120d3e47f626efe0fbbd7
kimsukdong <sdk1005@naver.com>
2021-12-02 11:32:10 +0900

    update image files

--- tablefx07/src/application/Main.java
+++ tablefx07/src/application/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -17,7 +18,9 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			primaryStage.setTitle("Student Record Magement");
+			primaryStage.setTitle("학생 통합 관리");
+			Image image = new Image(getClass().getResource("/images/hoseo2.png").toExternalForm());
+			primaryStage.getIcons().add(image);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
